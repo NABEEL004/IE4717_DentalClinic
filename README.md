d1cff811c49dcc41e33801677f7de32c2296971b
WillaiaHu <e200230@e.ntu.edu.sg>
2023-10-30 17:20:15 +0800

    Rename the create-password.js to create_password.js. And made several trivial changes.
    Try to avoid using hyphens in the name of files!

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 A full-stack web application using HTML, CSS, JS, PHP and MySQL
 
 Welcome to our clinic!
+
+QA:
+1. What does hamburger navigation do in index.html? and toggleMenu() function associated with onclick in index.html?
+
+2. In createaccount.html, why the input fields of re-type password is not aligned with other input fields?
+
+3. how 'hidden' and 'visible' are implemented?
--- password.js
+++ password.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var showPassword = document.getElementById("hidden");
-    var hidePassword = document.getElementById("visible");
-    var passwordField = document.getElementById("password");
+document.addEventListener("DOMContentLoaded", function () {
+  var showPassword = document.getElementById("hidden");
+  var hidePassword = document.getElementById("visible");
+  var passwordField = document.getElementById("password");
 
-    showPassword.addEventListener("click", function() {
-        passwordField.type = "text";
-        showPassword.style.display = "none";
-        hidePassword.style.display = "block";
-    })
-    hidePassword.addEventListener("click", function() {
-        passwordField.type = "password";
-        showPassword.style.display = "block";
-        hidePassword.style.display = "none";
-      }
-    )
-    });
+  showPassword.addEventListener("click", function () {
+    passwordField.type = "text";
+    showPassword.style.display = "none";
+    hidePassword.style.display = "block";
+  })
+  hidePassword.addEventListener("click", function () {
+    passwordField.type = "password";
+    showPassword.style.display = "block";
+    hidePassword.style.display = "none";
+  }
+  )
+});
